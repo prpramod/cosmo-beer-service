9ff9b5397cc5f3a67991b97a2af47506b1a5167e
pramodpra <pramod.pradhan@ephesoft.com>
2020-05-07 09:09:22 +0530

    Adding Junit tests for  Beer controller

---
+++ src/test/java/cosmo/springframework/cosmobeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,37 @@
+package cosmo.springframework.cosmobeerservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void getBeerById() throws Exception {
+
+        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeerById() {
+    }
+}
